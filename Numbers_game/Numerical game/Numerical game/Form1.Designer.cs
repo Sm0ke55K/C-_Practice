0ae33339f53cd5894fcb2da57f5562ef2a4b6664
Губанов Матвей <90284279+Sm0ke55K@users.noreply.github.com>
2022-07-21 00:17:55 +0300

    Add files via upload

---
+++ Numbers_game/Numerical game/Numerical game/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Numerical_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(417, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "число1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(417, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "число2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(393, 206);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 3);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(346, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 69);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "+";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Snow;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(343, 231);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 49);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(393, 286);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(290, 3);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(551, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Новый пример";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(390, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 45);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Проверить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.radioButton1.Location = new System.Drawing.Point(180, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 29);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1 - 5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.radioButton2.Location = new System.Drawing.Point(265, 22);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 29);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "1 - 10";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.radioButton3.Location = new System.Drawing.Point(365, 22);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(83, 29);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.Text = "1 - 25";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoEllipsis = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(22, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(310, 132);
+            this.label4.TabIndex = 12;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.radioButton4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.radioButton4.Location = new System.Drawing.Point(454, 22);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(83, 29);
+            this.radioButton4.TabIndex = 13;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "1 - 50";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(803, 402);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Numerical game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ Numbers_game/Numerical game/Numerical game/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numerical_game
+{
+    public partial class Form1 : Form
+    {
+        int answer;
+        string[] numbers = {"zero", "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
+                "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", 
+                "eighteen", "nineteen", "twenty", "twenty - one", "twenty - two",
+                "twenty - three", "twenty - four", "twenty - five", "twenty - six",
+                "twenty - seven", "twenty - eight", "twenty - nine", "thirty",
+                "thirty - one", "thirty - two", "thirty - three", "thirty - four",
+                "thirty - five", "thirty - six", "thirty - seven", "thirty - eight",
+                "thirty - nine", "forty", "forty - one", "forty - two", "forty - three",
+                "forty - four", "forty - five", "forty - six", "forty - seven",
+                "forty - eight", "forty - nine", "fifty", "fifty - one", "fifty - two",
+                "fifty - three", "fifty - four", "fifty - five", "fifty - six",
+                "fifty - seven", "fifty - eight", "fifty - nine", "sixty", "sixty - one",
+                "sixty - two", "sixty - three", "sixty - four", "sixty - five",
+                "sixty - six", "sixty - seven", "sixty - eight", "sixty - nine",
+                "seventy", "seventy - one", "seventy - two", "seventy - three",
+                "seventy - four", "seventy - five", "seventy - six", "seventy - seven",
+                "seventy - eight", "seventy - nine", "eighty", "eighty - one",
+                "eighty - two", "eighty - three", "eighty - four", "eighty - five",
+                "eighty - six", "eighty - seven", "eighty - eight", "eighty - nine",
+                "ninety", "ninety - one", "ninety - two", "ninety - three",
+                "ninety - four", "ninety - five", "ninety - six", "ninety - seven",
+                "ninety - eight", "ninety - nine", "hundred"};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1_Click(null, null);
+            textBox1.Focus();
+        }
+
+        void button2_Click(object sender, EventArgs e) // Проверить
+        {
+            string answer2 = textBox1.Text;
+            if (answer2 == numbers[answer])
+            {
+                label4.ForeColor = Color.Lime;
+                label4.Text = "Правильно!";
+            }
+            else
+            {
+                label4.ForeColor = Color.Red;
+                label4.Text = "Не правильно! :(";
+            }
+            textBox1.Focus();
+        }
+
+        void button1_Click(object sender, EventArgs e) // Новый пример
+        {
+            label4.Text = "";
+            textBox1.Text = "";
+            textBox1.Focus();
+
+            Random rnd = new Random();
+
+            if (radioButton1.Checked)
+            {
+                int value1 = rnd.Next(1, 6);
+                label1.Text = numbers[value1];
+                int value2 = rnd.Next(1, 6);
+                label2.Text = numbers[value2];
+                answer = value1 + value2;
+            }
+
+            if (radioButton2.Checked)
+            {
+                int value1 = rnd.Next(1, 11);
+                label1.Text = numbers[value1];
+                int value2 = rnd.Next(1, 11);
+                label2.Text = numbers[value2];
+                answer = value1 + value2;
+            }
+
+            if (radioButton3.Checked)
+            {
+                int value1 = rnd.Next(1, 26);
+                label1.Text = numbers[value1];
+                int value2 = rnd.Next(1, 26);
+                label2.Text = numbers[value2];
+                answer = value1 + value2;
+            }
+
+            if (radioButton4.Checked)
+            {
+                int value1 = rnd.Next(1, 51);
+                label1.Text = numbers[value1];
+                int value2 = rnd.Next(1, 51);
+                label2.Text = numbers[value2];
+                answer = value1 + value2;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                button2.Enabled = true;
+            else
+            {
+                label4.Text = "";
+                button2.Enabled = false;
+            }
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter & textBox1.Text != "")
+            {
+                e.SuppressKeyPress = true;
+                button2_Click(null, null);
+            }
+        }
+    }
+}
